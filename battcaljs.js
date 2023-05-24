8204f61eeacfc8c4be7e52d5faca89b6304e4b3e
DavidWangNait <savedan1999@gmail.com>
2023-05-24 15:16:24 -0700

    js

---
+++ battcaljs.js
@@ -0,0 +1,54 @@
+window.onload=()=>{
+    const button=document.querySelector("[name=cap]")
+    const v=document.querySelector("[name=vol]")
+    const a=document.querySelector("[name=ah]")
+    const c= document.querySelector("[name=custom]")
+    var cap;
+    v.onchange=()=>{
+        if(isNaN(v.value)|isNaN(a.value))//error
+        {
+            console.log("Error")
+        }
+        else//calculation
+        {
+            console.log(v.value)
+            console.log(a.value)
+            cap=parseInt(v.value)*parseInt(a.value)
+            console.log(cap)
+            document.querySelector("[name=cap]").value=cap
+            caltime();
+        }
+    }
+    a.onchange=()=>{
+        if(isNaN(v.value)|isNaN(a.value))//error
+        {
+            console.log("Error")
+        }
+        else//calculation
+        {
+            console.log(v.value)
+            console.log(a.value)
+            cap=parseInt(v.value)*parseInt(a.value)
+            console.log(cap)
+            document.querySelector("[name=cap]").value=cap
+            caltime();
+        }
+    }
+    button.onchange=()=>{
+        caltime();
+    }
+    c.onchange=()=>{
+        caltime();
+    }
+    function caltime(){
+      var cap=parseInt(document.querySelector("[name=cap]").value);
+      document.getElementById("phone").innerHTML=cap/15;
+      document.getElementById("LED").innerHTML=cap/9;
+      document.getElementById("laptop").innerHTML=cap/70;
+      document.getElementById("laptop2").innerHTML=cap/20;
+      document.getElementById("fan").innerHTML=cap/30;
+      document.getElementById("TV").innerHTML=cap/100;
+      document.getElementById("heater").innerHTML=cap/500;
+      document.getElementById("cust").innerHTML=cap/parseInt(document.querySelector("[name=custom]").value);
+    }
+    }
