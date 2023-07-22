3ea63434b9fc3a0fca7c4546859db3979897ac74
DavidWangNait <savedan1999@gmail.com>
2023-07-22 15:51:14 -0700

    nav

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var show=0;
+var barshow=0;
 
 window.onload=()=>{
     
@@ -17,6 +18,37 @@ window.onload=()=>{
 
             }
         }
-       
+    }
+    document.querySelector("#nav").onclick=()=>
+    {
+        if(barshow==0){
+            $("div").css("grid-column","2/3");
+            $("nav").fadeIn(1500);
+            barshow=1;
+        }
+        else{
+            barshow=2;
+            $("nav").fadeOut(1500,function(){
+                $("div").css("grid-column","1/3");barshow=0;
+            });
+            
+        }
+    }
+    document.querySelector("#skills").onclick=()=>{
+        $("#Myexp").fadeIn(1500);
+        $("#Mypro").fadeIn(1500);
+        document.querySelector("#Myskl").scrollIntoView();
+    }
+    document.querySelector("#experiences").onclick=()=>{
+        $("#Myexp").fadeIn(1500);
+        $("#Mypro").fadeIn(1500);
+        document.querySelector("#Myexp").scrollIntoView();
+    }
+    document.querySelector("#project").onclick=()=>{
+        $("#Myexp").fadeIn(1500);
+        $("#Mypro").fadeIn(1500);
+        document.querySelector("#Mypro").scrollIntoView();
     }
 }
+
+    
